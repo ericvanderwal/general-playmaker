24971fb39188572d12d3ae719e610fa25138b136
Eric Vander Wal <eric_vanderwal@hotmail.com>
2017-08-09 23:09:37 +0800

    Add files via upload

---
+++ Materials/startEnviromentUpdate.cs
@@ -0,0 +1,37 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.RenderSettings)]
+	[Tooltip("Schedules an update of the environment texture.")]
+	public class startEnviromentUpdate : FsmStateAction
+	{
+		[RequiredField]
+		public bool everyFrame;
+
+		public override void Reset()
+		{
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			DoUpdate();
+			
+			if (!everyFrame)
+				Finish();
+		}
+		
+		public override void OnUpdate()
+		{
+			DoUpdate();
+		}
+		
+		void DoUpdate()
+		{
+			DynamicGI.UpdateEnvironment();
+		}
+	}
+}
